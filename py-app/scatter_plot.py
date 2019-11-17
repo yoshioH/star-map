57d3d5e84592830e3615e8bad33c55bbb535006c
yoshioH <yoshio@hirayamahiroshitakenoMacBook-Pro.local>
2019-11-17 23:18:22 +0900

    add forgot files.

---
+++ py-app/scatter_plot.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+
+import calc
+import hip
+
+def lineToDatum(line):
+    datum = line.split('|')
+    tmp = {
+        'hipNum' : datum[1].strip(),
+        'vMag' : datum[5].strip(),
+        'raDeg' : datum[8].strip(),
+        'decDeg' : datum[9].strip(),
+        'parallax' : datum[11].strip(),
+        'bvColor' : datum[37].strip()
+    }
+    return tmp
+
+def vMagFilterAndParse(data):
+    ret = list()
+    for datum in data:
+        if len(datum['vMag']) < 1:
+            continue
+        try:
+            vMag = float(datum['vMag'])
+            bvColor = float(datum['bvColor'])
+            if bvColor > 2 : continue
+        except ValueError:
+            # パースに失敗した場合はスキップ
+            print(json.dumps(datum))
+            continue
+        tmp = {
+            'vMag':vMag,
+            'bvColor':bvColor
+        }
+        ret.append(tmp)
+    return ret
+
+def absMagFilterAndParse(data):
+    ret = list()
+    for datum in data:
+        if len(datum['vMag']) < 1 or len(datum['parallax']) < 1:
+            continue
+        try:
+            vMag = float(datum['vMag'])
+            parallax = float(datum['parallax'])
+            bvColor = float(datum['bvColor'])
+            parsec = calc.parallax_to_parsec(parallax)
+            absMag = calc.abs_mag_by_parsec(vMag, parsec)
+            # absMag = calc.parallax_to_abs_mag(vMag, parallax)
+            if bvColor > 2 : continue
+        except ValueError:
+            # パースに失敗した場合はスキップ
+            # print('value error : ' + json.dumps(datum))
+            continue
+        except ZeroDivisionError:
+            # print('zero divisions : ' + json.dumps(datum))
+            continue
+        tmp = {
+            'absMag':absMag,
+            'bvColor':bvColor
+        }
+        ret.append(tmp)
+    return ret
+
+if __name__ == '__main__':
+    data = hip.fileIn(lineToDatum, '../data/hip_main.dat')
+    vmagData = vMagFilterAndParse(data)
+    hip.jsonFileOut('../data/vmag_scatter-plot.json', vmagData)
+    absMagData = absMagFilterAndParse(data)
+    hip.jsonFileOut('../data/absmag_scatter-plot.json', absMagData)
+
---
+++ py-app/static_star_data.py
@@ -0,0 +1,65 @@
+Sun = {
+    'vMag' : -26.75,
+    'absMag': 4.82,
+    'parallax' : 0,
+    'bvColor': 0.65,
+    'name':'Sun',
+    'type_name':'主系列星'
+}
+Spica = {
+    'vMag' : 0.97,
+    'absMag': -3.5,
+    'parallax' : 13.06,
+    'bvColor':-0.23,
+    'name':'Spica',
+    'type_name':'青色巨星'
+}
+SiriusA = {
+    'vMag' : -1.46,
+    'absMag': 1.4,
+    'parallax' : 379.21,
+    'bvColor':-0.0,
+    'name':'SiriusA',
+    'type_name':'A型主系列星'
+}
+SiriusB = {
+    'vMag' : 8.44,
+    'absMag': 11.3,
+    'parallax' : 379.21,
+    'bvColor':-0.03,
+    'name':'SeriusB',
+    'type_name':'白色矮星'
+}
+Aldebaran = {
+    'vMag' : 0.86,
+    'absMag': 0.7,
+    'parallax' : 48.94,
+    'bvColor': 1.54,
+    'name':'Aldebaran',
+    'type_name':'赤色巨星'
+}
+Betelgeuse = {
+    'vMag' : 0.42,
+    'absMag': -5.5,
+    'parallax' : 642.53,
+    'bvColor': 1.85,
+    'name':'Betelgeuse',
+    'type_name':'赤色超巨星'
+}
+Pollux = {
+    'vMag' : 1.14,
+    'absMag': 1.1,
+    'parallax' : 96.54,
+    'bvColor': 1.0,
+    'name':'Pollux',
+    'type_name':'巨星'
+}
+
+ALL_LIST = list()
+ALL_LIST.append(Sun)
+ALL_LIST.append(Spica)
+ALL_LIST.append(SiriusA)
+ALL_LIST.append(SiriusB)
+ALL_LIST.append(Aldebaran)
+ALL_LIST.append(Betelgeuse)
+ALL_LIST.append(Pollux)
